a767e7bd3bec367969424e33744c6abfa0ca4531
Connectivista <alper.ozay28@gmail.com>
2026-06-01 22:37:39 +0300

    Delete fix.py

--- fix.py
+++
@@ -1,7 +0,0 @@
-with open('index.html', 'r') as f:
-    content = f.read()
-content = content.replace("http://localhost:8888/index.html", "https://connectivista.github.io/gesture-control/")
-content = content.replace("http://localhost:8888/callback", "https://connectivista.github.io/gesture-control/")
-with open('index.html', 'w') as f:
-    f.write(content)
-print("Bitti:", content.count("connectivista.github.io"))
